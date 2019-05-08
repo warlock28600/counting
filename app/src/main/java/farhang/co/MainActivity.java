b00704f3826e25a4ac7b27c6d56b285c780dba4d
farhang <farhang.ezziirani@gmail.com>
2019-05-08 21:07:44 +0430

    اینجکت view ها

--- app/src/main/java/farhang/co/MainActivity.java
+++ app/src/main/java/farhang/co/MainActivity.java
@@ -1,19 +1,40 @@
 package farhang.co;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button bt_equal;
 
     EditText tx_totalmoney,tx_totalmonth,tx_incompercent,tx_frontpercent;
+    TextView tx1,tx2,tx3,tx4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt_equal=findViewById(R.id.bt_equal);
+        tx_totalmoney=findViewById(R.id.tx_totalmoney);
+        tx_frontpercent=findViewById(R.id.tx_frontpercent);
+        tx_incompercent=findViewById(R.id.tx_incompercent);
+        tx_totalmonth=findViewById(R.id.tx_totalmonth);
+        tx1=findViewById(R.id.tx1);
+        tx2=findViewById(R.id.tx2);
+        tx3=findViewById(R.id.tx3);
+        tx4=findViewById(R.id.tx4);
+
+        bt_equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent());
+            }
+        });
+
     }
 }
