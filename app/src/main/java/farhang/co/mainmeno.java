2bde0a754e2bab27b58a6a8da3b35c215ff9b956
farhang <farhang.ezziirani@gmail.com>
2019-05-21 22:56:29 +0430

    mainmeno desin

---
+++ app/src/main/java/farhang/co/mainmeno.java
@@ -0,0 +1,14 @@
+package farhang.co;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainmeno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainmeno);
+    }
+}
